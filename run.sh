5d4b4293eb4c78147516ddeefe9dac477c6fd43a
lllll680 <ly2002909@163.com>
2025-12-10 19:19:27 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+python /data2/ly/dataset_eval/code_apply/vendi_score_execution_records.py \
+  --data-roots /data2/ly/dataset_eval/code_apply/your_dir1 /data2/ly/dataset_eval/code_apply/your_dir2 \
+  --model-path /data2/Qwen/Qwen3-Embedding-0.6B \
+  --batch-size 8
