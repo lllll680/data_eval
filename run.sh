42ec3ab9bb0ec2d1a6cc3bc71ca23bb47a2b1aa9
lllll680 <ly2002909@163.com>
2025-12-11 09:43:55 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,3 +2,10 @@ python /data2/ly/dataset_eval/code_apply/vendi_score_execution_records.py \
   --data-roots /data2/ly/dataset_eval/code_apply/your_dir1 /data2/ly/dataset_eval/code_apply/your_dir2 \
   --model-path /data2/Qwen/Qwen3-Embedding-0.6B \
   --batch-size 8
+
+
+   python vendi_score_analysis.py --data-roots /your/data/path
+
+      python vendi_score_execution_records.py \
+       --data-roots /your/data/path \
+       --include-values
