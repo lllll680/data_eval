f293a6c71a5aa10f0b89f83a6a7e143c27c83056
lllll680 <ly2002909@163.com>
2026-01-06 17:50:39 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,3 +12,9 @@ python /data2/ly/dataset_eval/code_apply/vendi_score_execution_records.py \
 
 
 screen -L -S swift_8b -Logfile /raid/data/ly/data/agentgym_data_new/swift.log bash /raid/data/ly/data/agentgym_data_new/swift_qwen3_8b.sh
+
+
+python vendi_score_execution_records.py \
+    --data-roots /raid/data/ly/data /raid/data/ly/data2 /raid/data/ly/data3 \
+    --model-path /data2/Qwen/Qwen3-Embedding-0.6B \
+    --batch-size 8
